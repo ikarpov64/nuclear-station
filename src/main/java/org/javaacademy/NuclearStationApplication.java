a02d38604109ca25a991c302af8f39940eda6945
i.karpov <tri3xxx@yandex.ru>
2024-03-21 10:09:56 +0400

    Удаление закоментированного кода в NuclearStationApplication

--- src/main/java/org/javaacademy/NuclearStationApplication.java
+++ src/main/java/org/javaacademy/NuclearStationApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NuclearStationApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NuclearStationApplication.class, args);
 	}
